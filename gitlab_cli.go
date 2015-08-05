3b1dc77028ba7d93854abf50b2cb0dd312ffafc7
Brandon Powell <bpowell29a@gmail.com>
2015-08-04 21:03:46 -0400

    grabs all projects

--- gitlab_cli.go
+++ gitlab_cli.go
@@ -13,6 +13,12 @@ type Configuration struct {
 	PrivateKey string
 }
 
+type Project struct {
+	Creator_id int
+	Id         int
+	Name       string
+}
+
 const (
 	APIPATH = "/api/v3/projects"
 	PRIVKEY = "private_token="
@@ -34,6 +40,31 @@ func NewConfig(filename string) (Configuration, error) {
 	return config, nil
 }
 
+func GetAllProjects(url string) ([]Project, error) {
+	var projects []Project
+	r, err := http.Get(url)
+	body, err := ioutil.ReadAll(r.Body)
+
+	var list []map[string]interface{}
+	err = json.Unmarshal(body, &list)
+	if err != nil {
+		return projects, err
+	}
+
+	for _, data := range list {
+		d, _ := json.Marshal(data)
+		var p Project
+		err = json.Unmarshal(d, &p)
+		if err != nil {
+			return projects, err
+		}
+
+		projects = append(projects, p)
+	}
+
+	return projects, nil
+}
+
 func main() {
 	config, err := NewConfig("config.json")
 	if err != nil {
@@ -43,7 +74,12 @@ func main() {
 
 	fmt.Println(config)
 
-	r, err := http.Get(fmt.Sprintf("%s%s?%s%s", config.Server, APIPATH, PRIVKEY, config.PrivateKey))
-	body, err := ioutil.ReadAll(r.Body)
-	fmt.Println(body)
+	url := fmt.Sprintf("%s%s?%s%s", config.Server, APIPATH, PRIVKEY, config.PrivateKey)
+	projects, err := GetAllProjects(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(projects)
 }
